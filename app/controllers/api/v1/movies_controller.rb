7e9c7531ffdee145514e468de9760a475b69a1a1
jeff-gosselin <jgoz.email@gmail.com>
2019-03-04 17:50:29 -0500

    database storing data from api

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,11 +1,7 @@
 class Api::V1::MoviesController < ApplicationController
 	def index
-		@latest = Tmdb::Movie.latest
-		@upcoming = Tmdb::Movie.upcoming
-		@now_playing = Tmdb::Movie.now_playing
-		@top_rated = Tmdb::Movie.top_rated
-		@popular = Tmdb::Movie.popular
-		render json: [latest: @latest, upcoming: @upcoming, now_playing: @now_playing, top_rated: @top_rated, popular: @popular]
+		@movies = Movie.all
+		render json: @movies
 	end
 
 	def latest
