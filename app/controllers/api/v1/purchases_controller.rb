79d3ddedc222a7d29a8832269799ceeaf1937ddb
jeff-gosselin <jgoz.email@gmail.com>
2019-03-07 14:22:37 -0500

    still wip

--- app/controllers/api/v1/purchases_controller.rb
+++ app/controllers/api/v1/purchases_controller.rb
@@ -8,4 +8,16 @@ class Api::V1::PurchasesController < ApplicationController
 		@purchase = Purchase.find(params[:id])
 		render :json => @purchase, each_serializer: PurchasesSerializer
 	end
+
+	def create
+		@purchase = Purchase.create(purchase_params)
+		render :json => @purchase, each_serializer: PurchaseSerializer
+	end
+
+	private
+
+	def purchase_params
+		params.require(:purchase).permit(:movie_id, :user_id)
+	end
+
 end
