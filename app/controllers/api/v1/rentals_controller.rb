cf0b8f01d13f4f2d523979b479687a09366193bd
jeff-gosselin <jgoz.email@gmail.com>
2019-03-07 09:33:57 -0500

    rentals are posting

--- app/controllers/api/v1/rentals_controller.rb
+++ app/controllers/api/v1/rentals_controller.rb
@@ -1,17 +1,17 @@
 class Api::V1::RentalsController < ApplicationController
 	def index
 		@rentals = Rental.all
-		render :json => @rentals, each_serializer: RentalsSerializer
+		render :json => @rentals, each_serializer: RentalSerializer
 	end
 
 	def show
 		@rental = Tmdb::Movie.detail(params[:id])
-		render :json => @rental, each_serializer: RentalsSerializer
+		render :json => @rental, each_serializer: RentalSerializer
 	end
 
 	def create
     @rental = Rental.create(rental_params)
-    render :json => @rental, each_serializer: RentalsSerializer
+    render :json => @rental, each_serializer: RentalSerializer
   end
 
 	def destroy
@@ -23,7 +23,7 @@ class Api::V1::RentalsController < ApplicationController
 
 	private
 	def rental_params
-		params.require(:rentals).permit(:movie_id, :user_id)
+		params.require(:rental).permit(:movie_id, :user_id)
 	end
 
 end
