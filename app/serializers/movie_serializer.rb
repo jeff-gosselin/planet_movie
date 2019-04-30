fd58aad2daa7badf687bed78d5b51604aa94a79d
Jeff <jgoz.email@gmail.com>
2019-04-30 01:50:49 -0400

    images now appear

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,5 +1,5 @@
 class MovieSerializer < ActiveModel::Serializer
-  attributes :id, :title, :overview, :release_date, :vote_average, :category
+  attributes :id, :title, :overview, :release_date, :vote_average, :category, :poster_path, :backdrop_path
   has_many :listings
   has_many :users, through: :listings
 
